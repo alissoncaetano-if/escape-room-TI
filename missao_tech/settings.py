78a7406d82a256c651c6a9c3e004916018fb32dd
Alisson Caetano <alisson.caetano@aluno.ifsertao-pe.edu.br>
2025-12-04 21:48:17 -0300

    criacao das urls dos aplicativos e criacao do template base

--- missao_tech/settings.py
+++ missao_tech/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Aplicativos
     'apps.core',
     'apps.excel',
     'apps.hardware',
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'missao_tech.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
